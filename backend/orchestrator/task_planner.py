ce59a59707c5047a532cffe9ae2a3d4a76aee3c6
Elliot cat <xiyuan23@g.ucla.edu>
2026-05-21 19:53:11 -0700

    added task object def

--- backend/orchestrator/task_planner.py
+++ backend/orchestrator/task_planner.py
@@ -1,17 +1,56 @@
 # takes the raw llm output and converts it into ordered plan_steps
 # each step is like {tool, params, status} that the worker can execute
 from backend.models.datatypes import TaskType
+from enum import Enum
+import json
+from datetime import datetime, timezone # UTC time stamp
+from typing import Any
+from uuid import UUID
 
+
+class Status(Enum):
+    PENDING= 1
+    IN_PROGRESS=2
+    ESCALATION_PENDING=3
+    COMPLETED=4
+    FAILED=5
+    
 class TaskPlanner:
     def __init__(self, llm_adapter):
         self.llm_adapter = llm_adapter
-    
+
+
     class StructuredTaskPlan:
         def __init__(self, task_type: TaskType, description: str, plan_steps: list, response_message: str):
             self.task_type = task_type
             self.description = description
             self.plan_steps = plan_steps
             self.response_message = response_message
+
+    class task:
+        
+
+        def __init__(self, id: UUID, user_id: UUID, status: Status, type: str, description: str, plan_steps: list[dict[str, Any]], escalation_deadline: datetime| None, created_at:datetime, updated_at: datetime) -> None:
+            """
+            Note: instead of json, I used a list for plan steps since it now makes mor esense to use taht. 
+            """
+            self.id = id
+            self.user_id = user_id
+            self.status = status
+            self.type = type
+            self.description = description
+            self.plan_steps=plan_steps
+            self.escalation_deadline = escalation_deadline
+            self.created_at = created_at
+            self.updated_at = updated_at
+        
+        def change_status(self, new_stat:Status):
+            # TODO: no guard unsafe should change
+            self.status = new_stat
+
+
+
+            
             
 
     def create_task_plan(self, query: str, context: dict = None, intent: str = None) -> list:
@@ -19,6 +58,8 @@ class TaskPlanner:
         # depending on the intent type, we might want to create different half manuallly constructed pipelines along with different system prompts to guide the llm to output the right format for each intent
         # for example, if intent is calendar_update, we might want to have a system prompt that specifically tells the llm to output a plan that involves calendar_tool, and we might want to pre-fill some of the params for the calendar_tool based on the context
         # also, the return format would depend on the intent.
+
+
 
         
         
