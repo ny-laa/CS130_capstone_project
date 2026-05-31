f847fb45303eb93394203c0286b2c7c98639227c
Ashita <ashisin17@gmail.com>
2026-05-31 14:38:29 -0700

    Auth frontend flow

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar';
 import Profile from './pages/Profile';
 import Conversations from './pages/Conversations';
 import Tasks from './pages/Tasks';
+import Register from './pages/Register';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <main className="main-content">
           <Routes>
             <Route path="/" element={<Navigate to="/tasks" replace />} />
+            <Route path="/register" element={<Register />} />
             <Route path="/tasks" element={<Tasks />} />
             <Route path="/conversations" element={<Conversations />} />
             <Route path="/profile" element={<Profile />} />
